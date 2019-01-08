9b1b28efafdf4943d5bf5920efea8702557101f3
elpon <kallipolo@gmail.com>
2019-01-08 18:06:50 +0900

    1. 이동 버튼 주변에서 스와이프 안 되게 바꿈

---
+++ Assets/Scripts/Button/NonSwipeArea.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NonSwipeArea : MonoBehaviour {
+
+    public TouchCameraInGame TCIG;
+
+    // Use this for initialization
+    void Start()
+    {
+        EventTrigger eventTrigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry_PointerDown = new EventTrigger.Entry();
+        EventTrigger.Entry entry_PointerUp = new EventTrigger.Entry();
+        entry_PointerDown.eventID = EventTriggerType.PointerDown;
+        entry_PointerUp.eventID = EventTriggerType.PointerUp;
+        entry_PointerDown.callback.AddListener((data) => { OnPointerDown((PointerEventData)data); });
+        entry_PointerUp.callback.AddListener((data) => { OnPointerUp((PointerEventData)data); });
+        eventTrigger.triggers.Add(entry_PointerDown);
+        eventTrigger.triggers.Add(entry_PointerUp);
+    }
+
+    void OnPointerUp(PointerEventData data)
+    {
+        TCIG.nonbutton = false;
+    }
+
+    void OnPointerDown(PointerEventData data)
+    {
+        TCIG.nonbutton = true;
+    }
+}
